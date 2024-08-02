ca8210d5941f207186015e37fd421fdf595f9fc7
SummerSanches <103926258+SummerSanches@users.noreply.github.com>
2024-08-03 03:57:37 +0800

    Update AppShell.xaml.cs

--- MAUICalculator/AppShell.xaml.cs
+++ MAUICalculator/AppShell.xaml.cs
@@ -1,10 +1,15 @@
 ﻿namespace MAUICalculator
+using System;
+using Microsoft.Maui.Controls;
+
+namespace YourNamespace
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute(nameof(SubPage), typeof(SubPage));
         }
     }
 }
