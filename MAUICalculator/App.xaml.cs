3f16f3a461483da2d7b1e1d1aad9e28639b6e99f
SummerSanches <103926258+SummerSanches@users.noreply.github.com>
2024-08-03 03:58:01 +0800

    Update App.xaml.cs

--- MAUICalculator/App.xaml.cs
+++ MAUICalculator/App.xaml.cs
@@ -1,11 +1,13 @@
 ﻿namespace MAUICalculator
+using Microsoft.Maui.Controls;
+
+namespace YourNamespace
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
-
             MainPage = new AppShell();
         }
     }
