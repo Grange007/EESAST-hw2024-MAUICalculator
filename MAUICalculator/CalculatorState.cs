a1c623b63b814e374fb76b53d18ab089c40d9e20
xinliHuaweiLaptop <sun-xl18@tsinghua.org.cn>
2024-08-09 22:57:29 +0800

    Finish MAUI Homework

---
+++ MAUICalculator/CalculatorState.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MAUICalculator
+{
+    public static class CalculatorState
+    {
+        public static double currentNumber { get; set; } = 0;
+        public static double lastNumber { get; set; } = 0;
+        public static string currentOperator { get; set; } = "";
+        public static bool isResult { get; set; }=false;
+    }
+}
